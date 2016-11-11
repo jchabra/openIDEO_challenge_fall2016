7db55d194d1eaa89d3c437285f760f33eaa85e18
spencercap <cappiello.spencer@gmail.com>
2016-11-10 21:21:08 -0500

    added BankerBot styling
    to become the bank bot, type ‘bankerbot’ as a message and send ==> you
    now have new botMessage class styling

--- 02_bankerbot/app.js
+++ 02_bankerbot/app.js
@@ -1,58 +1,64 @@
+/*** Initialize ***/
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io').listen(http);
 
 var players = [];
+var botIP ;
 
-//Create a static file server
-// app.configure(function() {
-//   app.use(express.static(__dirname + '/public'));
-// });
 
-// app.get('/', function(req, res) {
-//      res.sendFile('public/index.html');
-//  });
-
+/** fire up the HTML ***/
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 
-  // res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 
+/*** SocketIO MAGIC ***/
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
 
-  // Handle PLAYERS
-  var address = socket.handshake.address;
-  var ip = address.slice(7);
-  //console.log(ip);
+  /*** Handle PLAYERS ***/
+  var ip = socket.handshake.address.slice(7);
   players.push(ip);
-  //console.log(players);
   console.log(toUnique(players));
 
 
+  /*** Handle MESSAGES ***/
+  socket.on('chat message', function(msg){
+
+    /* who's the bot? */
+    var messIP = socket.handshake.address.slice(7);
+    if ( messIP == botIP ) {
+      io.emit('bot message', msg);
+    }
+    else {
+      io.emit('chat message', msg);
+    }
+
+    /* set the bankerbot */
+    if (msg.toLowerCase() == 'bankerbot') {
+      botIP = messIP ;
+      console.log(botIP + ' is now the bot');
+    }
+
+  });
+
 });
 
-//Get the dummy data
-// require('./server/ddata.js');
-var port = process.env.PORT || 8080;
 
+
+/*** SETUP PORT ***/
+var port = process.env.PORT || 8080;
 http.listen(port, function(){
   console.log('listening on *' + port);
 });
-//
-// var port = 3000;
-// app.listen(port);
-// console.log('Express server started on port %s', port);
 
 
-// make a unique array (no duplicate IPs)
-function toUnique(a,b,c){//array,placeholder,placeholder
- b=a.length;
- while(c=--b)while(c--)a[b]!==a[c]||a.splice(c,1);
- return a;
+/*** FUNCTIONS ***/
+function toUnique(a,b,c){
+  // makes an array with duplicates ( array,placeholder,placeholder )
+  b=a.length;
+  while(c=--b)while(c--)a[b]!==a[c]||a.splice(c,1);
+  return a;
 }
