c82876d18e31174a1d6044eee6bbeb6cf033e50d
spencercap <cappiello.spencer@gmail.com>
2016-11-14 14:26:39 -0500

    support for static assets like CSS + images + favicon added
    ha - ra !

---
+++ 03_bankerbot/server.js
@@ -0,0 +1,61 @@
+/*** Initialize ***/
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io').listen(http);
+var path = require('path');
+
+var players = [];
+var botIP ;
+
+/* serve up the static assets folder (public) */
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+/*** SocketIO MAGIC ***/
+io.on('connection', function(socket){
+
+  /*** Handle PLAYERS ***/
+  var ip = socket.handshake.address.slice(7);
+  players.push(ip);
+  console.log(toUnique(players));
+
+
+  /*** Handle MESSAGES ***/
+  socket.on('chat message', function(msg){
+
+    /* who's the bot? */
+    var messIP = socket.handshake.address.slice(7);
+    if ( messIP == botIP ) {
+      io.emit('bot message', msg);
+    }
+    else {
+      io.emit('chat message', msg);
+    }
+
+    /* set the bankerbot */
+    if (msg.toLowerCase() == 'bankerbot') {
+      botIP = messIP ;
+      console.log(botIP + ' is now the bot');
+    }
+
+  });
+
+});
+
+
+
+/*** SETUP PORT ***/
+var port = process.env.PORT || 8080;
+http.listen(port, function(){
+  console.log('listening on *' + port);
+});
+
+
+/*** FUNCTIONS ***/
+function toUnique(a,b,c){
+  // makes an array with duplicates ( array,placeholder,placeholder )
+  b=a.length;
+  while(c=--b)while(c--)a[b]!==a[c]||a.splice(c,1);
+  return a;
+}
