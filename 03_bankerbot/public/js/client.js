6cf329397fb677ef50ee01c94e0392bd3bd3acb9
Jasmine <jchabra@gmail.com>
2016-11-29 13:06:02 -0500

    added new styling, colors, fonts, and header

--- 03_bankerbot/public/js/client.js
+++ 03_bankerbot/public/js/client.js
@@ -5,8 +5,8 @@ $('form').submit(function(){
   return false;
 });
 socket.on('chat message', function(msg){
-  $('#messages').append($('<li class="userMessage grid-70 mobile-grid-70 prefix-30">').text(msg));
+  $('#messages').append($('<li class="grid-70 mobile-grid-70 prefix-30 userMessage">').text(msg));
 });
 socket.on('bot message', function(msg){
-  $('#messages').append($('<li class="botMessage">').text(msg));
+  $('#messages').append($('<li class="grid-70 mobile-grid-70 suffix-30 botMessage">').text(msg));
 });
