1d3d1ecd5d1a7d492c31c1a51a26df1d6f354f20
Jasmine <jchabra@gmail.com>
2016-11-29 12:07:34 -0500

    initial styling

--- 03_bankerbot/public/js/client.js
+++ 03_bankerbot/public/js/client.js
@@ -5,7 +5,7 @@ $('form').submit(function(){
   return false;
 });
 socket.on('chat message', function(msg){
-  $('#messages').append($('<li>').text(msg));
+  $('#messages').append($('<li class="userMessage grid-70 mobile-grid-70 prefix-30">').text(msg));
 });
 socket.on('bot message', function(msg){
   $('#messages').append($('<li class="botMessage">').text(msg));
